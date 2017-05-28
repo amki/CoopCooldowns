25ba42c15fbe6b3dc3101fe83914c530453b95c3
amki <amki@amki.eu>
2017-05-28 23:38:25 +0200

    Cooldown spinner works \o/

--- CoopCooldowns.lua
+++ CoopCooldowns.lua
@@ -1,7 +1,109 @@
 --version 0.0.1
 
 local Users = {}
+local CooldownSpells = {
+	-- Mage
+	-- Arcane
+	[62] = {},
+	-- Fire
+	[63] = {},
+	-- Frost
+	[64] = {},
+
+	-- Paladin
+	-- Holy
+	[65] = {},
+	-- Protection
+	[66] = {},
+	-- Retribution
+	[70] = {},
+
+	-- Warrior
+	-- Arms
+	[71] = {},
+	-- Fury
+	[72] = {},
+	-- Protection
+	[73] = {},
+
+	-- Druid
+	-- Balance
+	[102] = {},
+	-- Feral
+	[103] = {},
+	-- Guardian
+	[104] = {},
+	-- Restoration
+	[105] = {
+		[48438] = true,
+		[22812] = true,
+		[18562] = true
+	},
+
+	-- Death Knight
+	-- Blood
+	[250] = {},
+	-- Frost
+	[251] = {},
+	-- Unholy
+	[252] = {},
+
+	-- Hunter
+	-- Beast Mastery
+	[253] = {},
+	-- Marksmanship
+	[254] = {},
+	-- Survival
+	[255] = {},
+
+	-- Priest
+	-- Discipline
+	[256] = {},
+	-- Holy
+	[257] = {},
+	-- Shadow
+	[258] = {},
+
+	-- Rogue
+	-- Assasination
+	[259] = {},
+	-- Outlaw
+	[260] = {},
+	-- Sublety
+	[261] = {},
+
+	-- Shaman
+	-- Elemental
+	[262] = {},
+	-- Enhancement
+	[263] = {},
+	-- Restoration
+	[264] = {},
+
+	-- Warlock
+	-- Affliction
+	[265] = {},
+	-- Demonology
+	[266] = {},
+	-- Destruction
+	[267] = {},
+
+	-- Monk
+	-- Brewmaster
+	[268] = {},
+	-- Windwalker
+	[269] = {},
+	-- Mistweaver
+	[270] = {},
+
+	-- Demon Hunter
+	-- Havoc
+	[577] = {},
+	-- Vengeance
+	[581] = {}
+}
 local playerUser = GetUnitName("player",true).."-"..GetRealmName()
+local iconSize = 32
 
 local CoopFrame = CreateFrame("Frame", "CoopFrame", UIParent)
 CoopFrame:RegisterEvent("SPELL_UPDATE_COOLDOWN")
@@ -14,10 +116,10 @@ CoopFrame:RegisterEvent("ADDON_LOADED")
 
 CoopFrame:ClearAllPoints()
 CoopFrame:SetPoint("CENTER")
-CoopFrame:SetSize(80, 80)
+CoopFrame:SetSize(iconSize, iconSize)
 CoopFrame.text = CoopFrame:CreateFontString(nil, "BACKGROUND", "PVPInfoTextFont")
 CoopFrame.text:SetAllPoints()
-CoopFrame.text:SetTextHeight(13)
+CoopFrame.text:SetTextHeight(12)
 CoopFrame:SetAlpha(1)
 CoopFrame:SetMovable(true)
 CoopFrame:EnableMouse(true)
@@ -25,14 +127,21 @@ CoopFrame:RegisterForDrag("LeftButton")
 CoopFrame:SetScript("OnDragStart", CoopFrame.StartMoving)
 CoopFrame:SetScript("OnDragStop", CoopFrame.StopMovingOrSizing)
 
-local abilityTexture = CoopFrame:CreateTexture()
-abilityTexture:SetAllPoints()
-abilityTexture:SetTexture("Interface\\Icons\\Ability_Druid_Flourish")
-local myCooldown = CreateFrame("Cooldown", "myCooldown", CoopFrame, "CooldownFrameTemplate")
-myCooldown:SetAllPoints()
-
 function table.pack(...)
   return { n = select("#", ...), ... }
+end
+
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
 end
 
 CoopFrame:SetScript("OnEvent", function(self, event_name, ...)
@@ -42,8 +151,8 @@ CoopFrame:SetScript("OnEvent", function(self, event_name, ...)
 end)
 
 function CoopFrame:ADDON_LOADED(event,addon)
-	if addon == "CoopHelper" then
-		CoopFrame:RebuildTable()
+	if addon == "CoopCooldowns" then
+		--CoopFrame:RebuildTable()
 	end
 end
 
@@ -55,8 +164,9 @@ function CoopFrame:RebuildTable()
 	Users = {}
 	-- print("Reset Addon Users table")
 	if IsInGroup() or IsInRaid() or IsInGroup(LE_PARTY_CATEGORY_INSTANCE) then
-		local _,_,cIdx = UnitClass("player")
-		SendAddonMessage(MSG_PREFIX,"INIT;"..cIdx..";"..GetSpecialization(),RAID)
+		local playerSpecId, _, _, _, _, _ = GetSpecializationInfo(GetSpecialization())
+		print("I am spec "..playerSpecId)
+		SendAddonMessage(MSG_PREFIX,"INIT;"..playerSpecId,RAID)
 	end
 end
 
@@ -66,17 +176,69 @@ function CoopFrame:CHAT_MSG_ADDON(event,...)
 		return
 	end
 	if message:match("^INIT") then
-		local msg, class, spec = message:match("([^;]+);([^;]+);([^;]+)")
-		print("Sender "..sender.." sent "..message)
-		Users[sender] = {classId = class, specId = spec}
-		print("User "..sender.." has specId "..Users[sender].specId.." and "..Users[sender].classId)
+		local msg, specId = message:match("([^;]+);([^;]+)")
+		Users[sender] = {specId = specId, cdIcons = {}}
+		print("User "..sender.." has specId "..Users[sender].specId)
+		CoopFrame:CreateIcons()
+	elseif message:match("^SUP") then
+		local msg, spellId, start, duration, enabled = message:match("([^;]+);([^;]+);([^;]+);([^;]+)")
+		print("Spellupdate: "..spellId.." s: "..start.." d: "..duration)
+		if Users[sender] == nil then
+			print("ERROR: Got SUP from someone who was not initiated.")
+			return
+		end
+		local f = Users[sender]["cdIcons"][tonumber(spellId)]
+		print(dump(Users[sender]))
+		if f ~= nil then
+			f:SetCooldown(start,duration)
+		else
+			print("ERROR: Update for non-existing spell icon")
+		end
+	end
+end
+
+function CoopFrame:CreateIcons()
+	local lastFrame = nil
+	for name,obj in pairs(Users) do
+		print("User "..name.." has spec "..obj.specId)
+		local cdspells = CooldownSpells[tonumber(obj.specId)]
+		for spellId,val in pairs(cdspells) do
+			print("spellId "..spellId)
+			local spellName, rank, icon, castTime, minRange, maxRange = GetSpellInfo(spellId)
+			print("spellName "..spellName)
+			local spellCooldown = CreateFrame("Cooldown", nil, CoopFrame, "CooldownFrameTemplate")
+			local tex = CoopFrame:CreateTexture()
+			tex:SetTexture(icon)
+			if lastFrame == nil then
+				print("FIRST FRAME")
+				spellCooldown:SetPoint("TOPLEFT",CoopFrame,"TOPLEFT",0,0)
+				spellCooldown:SetSize(iconSize,iconSize)
+				tex:SetPoint("TOPLEFT",CoopFrame,"TOPLEFT",0,0)
+				tex:SetSize(iconSize,iconSize)
+			else
+				print("NEXT FRAME")
+				spellCooldown:ClearAllPoints()
+				spellCooldown:SetPoint("TOPLEFT",lastFrame,"BOTTOMLEFT",0,-5)
+				spellCooldown:SetSize(iconSize,iconSize)
+				tex:ClearAllPoints()
+				tex:SetPoint("TOPLEFT",lastFrame,"BOTTOMLEFT",0,-5)
+				tex:SetSize(iconSize,iconSize)
+			end
+			lastFrame = spellCooldown
+			
+			Users[name]["cdIcons"][spellId] = spellCooldown
+		end
 	end
 end
 
 function CoopFrame:SPELL_UPDATE_COOLDOWN(event,...)
-	-- print("SPELL_UPDATE_COOLDOWN")
-	local start, duration = GetSpellCooldown(48438)
-	myCooldown:SetCooldown(start, duration)
+	print("SPELL_UPDATE_COOLDOWN")
+	local playerSpecId, _, _, _, _, _ = GetSpecializationInfo(GetSpecialization())
+	print("Player has spec: "..playerSpecId)
+	for spellId,val in pairs(CooldownSpells[playerSpecId]) do
+		local start, duration, enabled = GetSpellCooldown(spellId);
+		SendAddonMessage(MSG_PREFIX,"SUP;"..spellId..";"..start..";"..duration..";"..enabled,RAID)
+	end
 end
 
 function CoopFrame:SPELL_UPDATE_USABLE(event,...)
